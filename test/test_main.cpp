7770ec1f3c8a5175845f25c7c869b2ba4004acd5
bjs3118 <59967644+bjs3118@users.noreply.github.com>
2022-03-14 18:08:12 +0000

    basic testing enviroment set up and working

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -15,50 +15,32 @@
 //   Test multithreading?
 //   Test synchronisation memory issues
 
-void test_led_builtin_pin_number(void)
+void test_example(void)
+/*
+ * Example function for testing the test script
+ */
 {
-    TEST_ASSERT_EQUAL(13, LED_BUILTIN);
-}
-
-void test_led_state_high(void)
-{
-    digitalWrite(LED_BUILTIN, HIGH);
-    TEST_ASSERT_EQUAL(HIGH, digitalRead(LED_BUILTIN));
-}
-
-void test_led_state_low(void)
-{
-    digitalWrite(LED_BUILTIN, LOW);
-    TEST_ASSERT_EQUAL(LOW, digitalRead(LED_BUILTIN));
+    TEST_ASSERT_EQUAL(13, 13);
 }
 
 void setup()
+/*
+ * Function to set up testing enviroment and call all test we only want to run once and not continously in the loop
+ */
 {
-    // NOTE!!! Wait for >2 secs
-    // if board doesn't support software reset via Serial.DTR/RTS
+    // Delaying until device is ready
     delay(2000);
 
+    // Starting testing
     UNITY_BEGIN(); // IMPORTANT LINE!
-    RUN_TEST(test_led_builtin_pin_number);
 
-    pinMode(LED_BUILTIN, OUTPUT);
+    RUN_TEST(test_example);
+
+    // TODO: Add test here
+
+    UNITY_END(); // stop unit testing
 }
-
-uint8_t i = 0;
-uint8_t max_blinks = 5;
 
 void loop()
 {
-    if (i < max_blinks)
-    {
-        RUN_TEST(test_led_state_high);
-        delay(500);
-        RUN_TEST(test_led_state_low);
-        delay(500);
-        i++;
-    }
-    else if (i == max_blinks)
-    {
-        UNITY_END(); // stop unit testing
-    }
 }
