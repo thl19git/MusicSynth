662dedf36ef38a4466e184082bf02233bf815c5b
bjs3118 <59967644+bjs3118@users.noreply.github.com>
2022-03-14 18:30:30 +0000

    up to date and with examples

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <unity.h>
+#include "knob.h"
 
 // void setUp(void) {
 // // set stuff up here
@@ -23,6 +24,27 @@ void test_example(void)
     TEST_ASSERT_EQUAL(13, 13);
 }
 
+//////////////////////
+//// Knob Library ////
+//////////////////////
+
+void test_calculateAndAssignval(void)
+/*
+ * Unit tests for the knob class function calculateAndAssignval
+ */
+{
+    Knob knobTest(0);
+
+    // TODO: Testing all possible combinations of inputs using truth table from notes
+
+    // example:
+    TEST_ASSERT_EQUAL_INT8(0, knobTest.calculateAndAssignval(0, 0, 0, 0, 1));
+
+    // TODO: test inputs that are not 0 or 1 (edge case)
+
+    // TODO: add other cases you can think of
+}
+
 void setup()
 /*
  * Function to set up testing enviroment and call all test we only want to run once and not continously in the loop
@@ -35,6 +57,9 @@ void setup()
     UNITY_BEGIN(); // IMPORTANT LINE!
 
     RUN_TEST(test_example);
+
+    // Knob example
+    RUN_TEST(test_calculateAndAssignval);
 
     // TODO: Add test here
 
