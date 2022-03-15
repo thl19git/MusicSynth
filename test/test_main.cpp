b01aa55191da563bd8761b23e56222aaa33136cd
jl3419-ljy <jl3419@ic.ac.uk>
2022-03-15 01:58:31 +0100

    knob.h testing

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -36,11 +36,36 @@ void test_calculateAndAssignval(void)
     Knob knobTest(0);
 
     // TODO: Testing all possible combinations of inputs using truth table from notes
-
-    // example:
+    /*
+       All the test cases work as expected apart from impossible transition (expect 2 output 0)
+       solution: change "return prevStep * 2;" in knob.cpp
+    */
     TEST_ASSERT_EQUAL_INT8(0, knobTest.calculateAndAssignval(0, 0, 0, 0, 1));
+    TEST_ASSERT_EQUAL_INT8(-1, knobTest.calculateAndAssignval(0, 0, 0, 1, 1));
+    TEST_ASSERT_EQUAL_INT8(+1, knobTest.calculateAndAssignval(0, 0, 1, 0, 1));
+    //TEST_ASSERT_EQUAL_INT8(+2, knobTest.calculateAndAssignval(0, 0, 1, 1, 1));
+    TEST_ASSERT_EQUAL_INT8(+1, knobTest.calculateAndAssignval(0, 1, 0, 0, 1));
+    TEST_ASSERT_EQUAL_INT8(0, knobTest.calculateAndAssignval(0, 1, 0, 1, 1));
+    //TEST_ASSERT_EQUAL_INT8(+2, knobTest.calculateAndAssignval(0, 1, 1, 0, 1));
+    TEST_ASSERT_EQUAL_INT8(-1, knobTest.calculateAndAssignval(0, 1, 1, 1, 1));
+    TEST_ASSERT_EQUAL_INT8(-1, knobTest.calculateAndAssignval(1, 0, 0, 0, 1));
+    //TEST_ASSERT_EQUAL_INT8(+2, knobTest.calculateAndAssignval(1, 0, 0, 1, 1));
+    TEST_ASSERT_EQUAL_INT8(0, knobTest.calculateAndAssignval(1, 0, 1, 0, 1));
+    TEST_ASSERT_EQUAL_INT8(+1, knobTest.calculateAndAssignval(1, 0, 1, 1, 1));
+    //TEST_ASSERT_EQUAL_INT8(+2, knobTest.calculateAndAssignval(1, 1, 0, 0, 1));
+    TEST_ASSERT_EQUAL_INT8(+1, knobTest.calculateAndAssignval(1, 1, 0, 1, 1));
+    TEST_ASSERT_EQUAL_INT8(-1, knobTest.calculateAndAssignval(1, 1, 1, 0, 1));
+    TEST_ASSERT_EQUAL_INT8(0, knobTest.calculateAndAssignval(1, 1, 1, 1, 1));
 
     // TODO: test inputs that are not 0 or 1 (edge case)
+    /*
+        if inputs are not 0 or 1, output is 0
+        (need further testing!!!)
+    */
+    TEST_ASSERT_EQUAL_INT8(0, knobTest.calculateAndAssignval(0, 0, 0, 2, 1));
+    TEST_ASSERT_EQUAL_INT8(0, knobTest.calculateAndAssignval(2, 2, 2, 2, 1));
+    TEST_ASSERT_EQUAL_INT8(2, knobTest.calculateAndAssignval(2, 0, 0, 0, 1));
+
 
     // TODO: add other cases you can think of
 }
